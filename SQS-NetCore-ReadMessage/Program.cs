c4c7af5e12a5e28e2742740e8caf466f44bdb048
venugopal.thippaneni <venugopal.thippaneni@medicaldirector.com>
2020-05-04 15:18:15 +0530

    C# console app  to work with AWS SQS

---
+++ SQS-NetCore-ReadMessage/Program.cs
@@ -0,0 +1,67 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Net;
+
+namespace SQS_NetCore_ReadMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queueName = "SQS-Msg-Queue";
+            ReadAWSSQSQueueMessageAndDelete(queueName);
+            Console.ReadLine();
+        }
+
+        public static void ReadAWSSQSQueueMessageAndDelete(string queueName)
+        {
+            using (IAmazonSQS sqs = new AmazonSQSClient(RegionEndpoint.USEast1))
+            {
+                var queueUrl = sqs.GetQueueUrlAsync(queueName).Result.QueueUrl;
+                var readQueueMsgReq = new ReceiveMessageRequest(queueUrl);
+
+                var readMsgResp = sqs.ReceiveMessageAsync(readQueueMsgReq).Result;
+                if (readMsgResp.HttpStatusCode == HttpStatusCode.OK && readMsgResp.Messages.Count > 0)
+                {
+                    Console.WriteLine("Reading Messages \n");
+                    foreach (var message in readMsgResp.Messages)
+                    {
+                        Console.WriteLine("Message: \n");
+                        Console.WriteLine($" MessageId: {message.MessageId} \n");
+                        Console.WriteLine($" ReceiptHandle: {message.ReceiptHandle} \n");
+                        Console.WriteLine($" MD5OfBody: {message.MD5OfBody} \n");
+                        Console.WriteLine($" Body: {message.Body} \n");
+
+                        foreach (var attribute in message.Attributes)
+                        {
+                            Console.WriteLine("Attributes: \n");
+                            Console.WriteLine($" attribute name: {attribute.Key} \n");
+                            Console.WriteLine($" attribute value: {attribute.Value} \n");
+                        }
+
+                        var deleteMsgReq = new DeleteMessageRequest()
+                        {
+                            QueueUrl = queueUrl,
+                            ReceiptHandle = message.ReceiptHandle
+                        };
+                        Console.WriteLine("deleting Message \n");
+                        var deleteResp = sqs.DeleteMessageAsync(deleteMsgReq).Result;
+
+                        if (deleteResp.HttpStatusCode == HttpStatusCode.OK)
+                        {
+                            Console.WriteLine("Message deleted successfully \n");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No messages to read in the queue\n");
+                }
+            }
+        }
+    }
+}
---
+++ SQS-NetCore/Program.cs
@@ -0,0 +1,98 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace SQS_NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queueName = "SQS-Msg-Queue";
+            var queueList = GetAWSSQSQueues();
+            if (queueList != null)
+            {
+                var queueUrl = "https://sqs.us-east-1.amazonaws.com/536344005429/" + queueName;
+
+                if (queueList.Count == 0 || !queueList.Contains(queueUrl))
+                {
+                    var isQueueCreated = CreateAWSSQSQueue(queueName);
+                    Console.WriteLine("Creating AWS SQS Queue, {0} .......", queueName);
+                    if (isQueueCreated.HttpStatusCode == HttpStatusCode.OK)
+                    {
+                        Console.WriteLine("AWS SQS Queue Created Successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine("AWS SQS Queue Creation Failed");
+                    }
+                }
+                else {
+                    Console.WriteLine("AWS SQS already exists with name {0}", queueName);
+                    Console.WriteLine("Sending message to queue - {0}", queueName);
+
+                    var result = SendMessageToAWSQueue(queueUrl, "Hello this is test message");
+                    if (result)
+                    {
+                        Console.WriteLine("Message  sent to queue - {0}", queueName);
+                    }
+                }
+            }
+            else {
+                Console.WriteLine("Connecting to AWS SQS Queue Failed");
+            }
+            Console.ReadLine();
+        }
+
+        public static CreateQueueResponse CreateAWSSQSQueue(string queueName)
+        {
+            using (IAmazonSQS sqs = new AmazonSQSClient(RegionEndpoint.USEast1))
+            {
+                Console.WriteLine("AWS SQS Queue Creation Started.....");
+                var createQueueReq = new CreateQueueRequest(queueName);
+                return sqs.CreateQueueAsync(createQueueReq).Result;
+            }
+        }
+
+        public static List<string> GetAWSSQSQueues()
+        {
+            using (IAmazonSQS sqs = new AmazonSQSClient(RegionEndpoint.USEast1))
+            {
+                Console.WriteLine("Getting list of available sqs queues .....");
+                var listeQueueReq = new ListQueuesRequest();
+                var listQueueRes = sqs.ListQueuesAsync(listeQueueReq).Result;
+                if (listQueueRes.HttpStatusCode == HttpStatusCode.OK)
+                {
+                    return listQueueRes.QueueUrls;
+                }
+                else {
+                    return null;
+                }                
+            }
+        }
+
+        public static bool SendMessageToAWSQueue(string queueUrl, string message)
+        {
+            using (IAmazonSQS sqs = new AmazonSQSClient(RegionEndpoint.USEast1))
+            {
+                Console.WriteLine("Adding message to queue .....");
+                var sendMsgRequest = new SendMessageRequest() {
+                    QueueUrl = queueUrl,
+                    MessageBody = message
+                };
+                var sendMsgResp = sqs.SendMessageAsync(sendMsgRequest).Result;
+                if (sendMsgResp.HttpStatusCode == HttpStatusCode.OK)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
